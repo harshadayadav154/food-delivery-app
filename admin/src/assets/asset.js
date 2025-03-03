f0f58db5356e843532c5bfba0a5e439dcacd2efd
Harshada <hyadav8701@conestogac.on.ca>
2025-03-03 12:21:39 -0500

    first commit

---
+++ admin/src/assets/asset.js
@@ -0,0 +1,17 @@
+import logo from "./app_logo.jpg";
+import add_icon from "./add_icon.png";
+import order_icon from "./order.png";
+import profile_image from "./profile.png";
+import upload_area from "./upload.png";
+import parcel_icon from "./box.png";
+
+export const assets = {
+  logo,
+  add_icon,
+  order_icon,
+  profile_image,
+  upload_area,
+  parcel_icon,
+};
+
+export const url = "http://localhost:4000";
